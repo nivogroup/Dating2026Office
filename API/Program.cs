5ce229aaf66437913a958dedab5edc940a9910ef
Siddhartha Dikshit <nivogroup@gmail.com>
2026-03-24 12:14:23 +0530

    BasicFrontEnd-Angular

--- API/Program.cs
+++ API/Program.cs
@@ -10,8 +10,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+// For Angular to give the access
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+// HTTP Request Pipeline
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
