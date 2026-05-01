15ea4316fd96756e7bd3111a1e95fab9e54e7e12
Siddhartha Dikshit <nivogroup@gmail.com>
2026-05-01 13:16:03 +0530

    Registration

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,35 +1,102 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RegisterCreds, User } from '../../../types/user';
+import { Component, inject, output, signal } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { RegisterCreds } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
+import { TextInput } from '../../../shared/text-input/text-input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, TextInput],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
   // this is for the parents to child communication in this case Home to register
-  //membersFromHome = input.required<User[]>(); 
-  private accountService = inject(AccountService)
-   // this is for the child to parents communication in this case register to home
+  //membersFromHome = input.required<User[]>();
+  private accountService = inject(AccountService);
+  private router = inject(Router);
+  // this is for the child to parents communication in this case register to home
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
 
+  // For The Form Builder
+  private fb = inject(FormBuilder);
+  // For the register form extension
+  protected credentialsFrom: FormGroup;
+  protected profileForm: FormGroup;
+  protected currentStep = signal(1);
+  protected validationErrors = signal<string[]>([]);
+
+  constructor() {
+    this.credentialsFrom = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      displayName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+    });
+
+    this.profileForm = this.fb.group({
+      gender: ['male', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+    });
+    this.credentialsFrom.controls['password'].valueChanges.subscribe(() => {
+      this.credentialsFrom.controls['confirmPassword'].updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent;
+      if (!parent) return null;
+
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null : { passwordMismatch: true };
+    };
+  }
+
+  nextStep() {
+    if (this.credentialsFrom.valid) {
+      this.currentStep.update((prevStep) => prevStep + 1);
+    }
+  }
+
+  prevStep() {
+    this.currentStep.update((prevStep) => prevStep - 1);
+  }
+
+  getMaxDate() {
+    const today = new Date();
+    today.setFullYear(today.getFullYear() - 18);
+    return today.toISOString().split('T')[0];
+  }
 
   register() {
-    this.accountService.register(this.creds).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => console.log(error)
-    })
+    if (this.credentialsFrom.valid && this.profileForm.valid) {
+      const formData = { ...this.credentialsFrom.value, ...this.profileForm.value };
+      this.accountService.register(formData).subscribe({
+        next: () => {
+          this.router.navigateByUrl('/members')
+        },
+        error: (error) => {
+          console.log(error);
+          this.validationErrors.set(error);
+        }
+      });
+    }
   }
 
   cancel() {
     this.cancelRegister.emit(false);
   }
-
 }
--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -2,11 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute } from '@angular/router';
 import { Member, Photo } from '../../../types/member';
-import { ImageUpload } from "../../../shared/image-upload/image-upload";
+import { ImageUpload } from '../../../shared/image-upload/image-upload';
 import { AccountService } from '../../../core/services/account-service';
 import { User } from '../../../types/user';
-import { StarButton } from "../../../shared/star-button/star-button";
-import { DeleteButton } from "../../../shared/delete-button/delete-button";
+import { StarButton } from '../../../shared/star-button/star-button';
+import { DeleteButton } from '../../../shared/delete-button/delete-button';
 
 @Component({
   selector: 'app-member-photos',
@@ -23,61 +23,68 @@ export class MemberPhotos implements OnInit {
   // For setting up main photo we need user also
   protected accountService = inject(AccountService);
 
-
   ngOnInit(): void {
     const memberId = this.route.parent?.snapshot.paramMap.get('id');
-    if(memberId) {
+    if (memberId) {
       this.memberService.getMemberPhotos(memberId).subscribe({
-        next: photos => this.photos.set(photos)
-      })
+        next: (photos) => this.photos.set(photos),
+      });
     }
   }
 
   onUploadImage(file: File) {
     this.loading.set(true);
     this.memberService.uploadPhoto(file).subscribe({
-       next: photo => {
+      next: (photo) => {
         this.memberService.editMode.set(false);
         this.loading.set(false);
-        this.photos.update(photos => [...photos, photo]);
-       },
-       error: error => {
+        this.photos.update((photos) => [...photos, photo]);
+        if(!this.memberService.member()?.imageUrl) {
+          this.setMainLocalPhoto(photo);
+        }
+      },
+      error: (error) => {
         console.log('Error uploading image: ', error);
         this.loading.set(false);
-       }
-    })
+      },
+    });
   }
 
   // For setting up main photo
   setMainPhoto(photo: Photo) {
     this.memberService.setMainPhoto(photo).subscribe({
       next: () => {
-        const currentUser = this.accountService.currentUser();
-        if(currentUser) currentUser.imageUrl = photo.url;
-        this.accountService.setCurrentUser(currentUser as User);
-
-        this.memberService.member.update(member => ({
-          ...member, 
-          imageUrl: photo.url
-        })as Member)
-      }
-    })
+        this.setMainLocalPhoto(photo);
+      },
+    });
   }
 
   deletePhoto(photoId: number) {
     this.memberService.deletePhoto(photoId).subscribe({
       next: () => {
-        this.photos.update(photos => photos.filter(x => x.id !== photoId));
-      }
-    })
+        this.photos.update((photos) => photos.filter((x) => x.id !== photoId));
+      },
+    });
+  }
+
+  private setMainLocalPhoto(photo: Photo) {
+    const currentUser = this.accountService.currentUser();
+    if (currentUser) currentUser.imageUrl = photo.url;
+    this.accountService.setCurrentUser(currentUser as User);
+
+    this.memberService.member.update(
+      (member) =>
+        ({
+          ...member,
+          imageUrl: photo.url,
+        }) as Member,
+    );
   }
 
   // For the extra photos
-/*   get photoMocks() {
+  /*   get photoMocks() {
     return Array.from({length: 20}, (_, i) => ({
       url: '/user.png'
     }))
   }  */
-
-
 }
